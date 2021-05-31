1252ce6f63fae27f3958cb14a70a6127292673fa
Andres Senac <andres@senac.es>
2021-05-31 11:23:54 +0200

    command line tool for explaining queries at different stages

--- dataflow_parser.py
+++ dataflow_parser.py
@@ -3,40 +3,49 @@
 import sys
 import re
 
-nodes = []
-edges = set()
 
-current_node = None
 
-while 1:
-    line = sys.stdin.readline()
-    if not line:
-        break
+def generate_graph(lines, graph_label):
+    nodes = []
+    edges = set()
+    current_node = None
 
-    if line.startswith(' %'):
-        current_node = dict()
-        current_node['name'] = line[1:line.find(' ', 1)]
-        current_node['text'] = line[line.find('=', 2) + 1:]
-    elif line == '\n':
+    print (lines)
+
+    for line in lines:
+        if line.startswith('%'):
+            current_node = dict()
+            current_node['name'] = line[:line.find(' ')]
+            current_node['text'] = line[line.find('=', 1) + 1:]
+        elif len(line) == 0:
+            nodes.append(current_node)
+            current_node = None
+        else:
+            current_node['text'] += '\n' + line
+
+    if current_node is not None:
         nodes.append(current_node)
-    else:
-        current_node['text'] += line
 
-for n in nodes:
-    for d in re.findall(r'%\d+', n['text']):
-        edges.add((d, n['name']))
+    for n in nodes:
+        for d in re.findall(r'%\d+', n['text']):
+            edges.add((d, n['name']))
 
-def node(x):
-    return x.replace('%', 'node')
-def label(l):
-    return l.strip(' \n').replace('\n', '\\l').replace('"', '\\"').replace('|', '').replace('>', '\\>').replace('<', '\\<') + '\\l'
+    def node(x):
+        return x.replace('%', 'node')
+    def label(l):
+        return l.strip(' \n').replace('\n', '\\l').replace('"', '\\"').replace('|', '').replace('>', '\\>').replace('<', '\\<') + '\\l'
 
-sys.stdout.write('digraph G {\n')
+    sys.stdout.write('digraph G {\n    label="%s"\n' % (label(graph_label)))
 
-for n in nodes:
-    sys.stdout.write('    %s [shape = record, label="%s"]\n' % (node(n['name']), label(n['text'])))
+    for n in nodes:
+        sys.stdout.write('    %s [shape = record, label="%s"]\n' % (node(n['name']), label(n['text'])))
 
-for e in edges:
-    sys.stdout.write('    %s -> %s [label = "%s"]\n' % (node(e[0]), node(e[1]), label(e[0])))
+    for e in edges:
+        sys.stdout.write('    %s -> %s [label = "%s"]\n' % (node(e[0]), node(e[1]), label(e[0])))
 
-sys.stdout.write('}\n')
+    sys.stdout.write('}\n')
+
+
+if __name__ == '__main__':
+    lines = sys.stdin.readlines()
+    generate_graph(lines, 'stdin')
---
+++ explain_query.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import psycopg2
+import itertools
+import sys
+from dataflow_parser import generate_graph 
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=6875,
+    database="materialize",
+    user="materialize")
+
+with conn.cursor() as cursor:
+    for arg in sys.argv[1:]:
+        for mode in ["raw plan for", "decorrelated plan for", ""]:
+            query = "explain " + mode + " " + arg
+            cursor.execute(query)
+            row = cursor.fetchone()
+            generate_graph(row[0].splitlines(), query)
+
+conn.close()
